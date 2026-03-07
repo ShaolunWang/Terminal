22f35a2e5f4ed490e8129247a5332dfecb05e1a1
ShaolunWang <wang.shaolun@gmail.com>
2026-03-07 16:45:56 +0800

    refactor(termbuffer): change screen and buffer type, update line class
    now screen and scrolblacks both will be ArrayDeque;
    Line class will use ArrayList.
    
    added helpfer functions to get lines as String
    
    added helper functinos

--- src/main/kotlin/TerminalBuffer.kt
+++ src/main/kotlin/TerminalBuffer.kt
@@ -1,11 +1,22 @@
 package org.example
 
 class Line(val width: Int) {
-    private val cells: Array<Cell> = Array(width) { Cell.EMPTY }
+    public val cells: ArrayList<Cell> = ArrayList(MutableList(width) { Cell.EMPTY })
     operator fun get(col: Int): Cell = cells[col]
     operator fun set(col: Int, cell: Cell) {
         cells[col] = cell
     }
+
+
+    override fun toString(): String {
+        return buildString {
+            for (cell in cells) {
+                // use Unicode null if cell is null
+                append(cell.char ?: '\u0000')
+            }
+        }
+    }
+
 }
 
 class TerminalBuffer(val width: Int, val height: Int, val maxScrollBack: Int = 5000) {
@@ -15,11 +26,28 @@ class TerminalBuffer(val width: Int, val height: Int, val maxScrollBack: Int = 5
         require(maxScrollBack > 0) { "Max scroll back is negative: $maxScrollBack" }
     }
 
-    // using array since the screen is fixed size (for now!)
-    private val screen: Array<Line> = Array(height) { Line(width) }
-    private val scrollback: ArrayDeque<Line> = ArrayDeque()
-    private var cursorCol: Int = 0
-    private var cursorRow: Int = 0
+    // NOTE: I think for both of them intuitively ringbuffer sounds like a
+    // more intuitive data structure,
+    // However for the sake of simplicity I'd use ArrayDeque
+
+    private val screen: ArrayDeque<Line> = ArrayDeque<Line>(height).apply {
+        repeat(height) {
+            add(Line(width))
+        }
+    }
+    private val scrollback: ArrayDeque<Line> = ArrayDeque(maxScrollBack);
+    public fun getLineAsString(i: Int): String {
+        return screen[i].toString()
+    }
+
+    public fun getScrollBackSize(): Int {
+        return scrollback.size
+    }
+
+    public fun getScrollBackLineAsString(i: Int): String {
+        return scrollback[i].toString()
+    }
+
     var currentAttrs: CellAttr = CellAttr()
         private set
     private val cursor = Cursor(width, height)
